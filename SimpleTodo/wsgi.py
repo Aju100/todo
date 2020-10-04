f9fb1e7ee012a6e7a57ba831ff27ffb87fa94073
Saravanan Selvamohan <43172875+saravananselvamohan@users.noreply.github.com>
2020-10-04 12:47:53 +0530

    Added verified commit

--- SimpleTodo/wsgi.py
+++ SimpleTodo/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for csdojo project.
+ASGI config for {{ project_name }} project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/{{ docs_version }}/howto/deployment/asgi/
 """
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SimpleTodo.settings')
 
-application = get_wsgi_application()
+application = get_asgi_application()
